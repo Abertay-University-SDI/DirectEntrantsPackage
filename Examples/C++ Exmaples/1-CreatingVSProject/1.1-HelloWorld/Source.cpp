8c37a3f8c590a6e68d49bd1d7d74213cacceaa38
Christopher Acornley <c.acornley@abertay.ac.uk>
2022-10-14 13:55:05 +0100

    Updated source code to use namespace

--- Examples/C++ Exmaples/1-CreatingVSProject/1.1-HelloWorld/Source.cpp
+++ Examples/C++ Exmaples/1-CreatingVSProject/1.1-HelloWorld/Source.cpp
@@ -1,13 +1,16 @@
 // Include iostream library. This allows us use of cout and cin.
 #include <iostream>	
 
+// State we are using the standard C++ namespace.
+using namespace std;
+
 int main()
 {
 	// Print 'Hello World!'
-	std::cout << "Hello World!";
+	cout << "Hello World!";
 
 	// Wait for user to press a key
-	std::cin.get();	
+	cin.get();	
 
 	// End of main
 	return 0;						
